d18ee6a1bc61501d3b21a69e8623a303dccc057e
RenanKJ <rkjesus@inf.ufrgs.br>
2018-10-21 21:15:28 -0300

    Changing tabs order and language

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -9,8 +9,8 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
-  tab1Root = ContactPage;
-  tab2Root = HomePage;
+  tab1Root = HomePage;
+  tab2Root = ContactPage;
   tab3Root = AboutPage;
   
 
